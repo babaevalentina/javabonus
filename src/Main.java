74367d40ccc247b9f3ab2a1861849956b8df4fd8
Валентина Бабаева <valentina_babaeva@list.ru>
2022-06-25 22:15:01 +0300

    add all

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int y = 100; //начальный счет
+        int x = 300; //пополнение
+
+        if (x > 1000) {
+            System.out.println(y+x / 100);
+        } else {
+            System.out.println(y);
+        }
+    }
+}
